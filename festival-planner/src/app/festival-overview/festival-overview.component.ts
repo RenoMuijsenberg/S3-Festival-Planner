ae8939ffde6961e3b1cf809be4beb07be3a4f5c1
Reno Muijsenberg <490412@student.fontys.nl>
2022-09-12 10:54:48 +0200

    Frontend update

--- festival-planner/src/app/festival-overview/festival-overview.component.ts
+++ festival-planner/src/app/festival-overview/festival-overview.component.ts
@@ -10,5 +10,7 @@ export class FestivalOverviewComponent {
   festivals: Festival[] = [
     {name: "Hard CVLTR", date: "20-10-2022", timeFrame: "17.00-00.00", price: 20.00},
     {name: "Rebellion", date: "30-12-2022", timeFrame: "13.00-00.00", price: 50.00},
+    {name: "Test", date: "30-12-2022", timeFrame: "13.00-00.00", price: 50.00},
+    {name: "Test 2", date: "30-12-2022", timeFrame: "13.00-00.00", price: 50.00},
   ];
 }
