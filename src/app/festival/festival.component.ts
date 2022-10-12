4317b04f8189a6aaf6c49516ab53e8641267aa74
Muijsenberg,Reno R.F <r.muijsenberg@student.fontys.nl>
2022-10-12 09:38:50 +0200

    Fix eslint errors

--- src/app/festival/festival.component.ts
+++ src/app/festival/festival.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 import {Festival} from "../../interfaces/festival.interface";
 
 @Component({
-  selector: 'festival',
+  selector: 'app-festival',
   templateUrl: './festival.component.html',
   styleUrls: ['./festival.component.css']
 })
