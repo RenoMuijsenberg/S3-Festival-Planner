d404c9e9fa4cc97dbea31059e599fbbcee8b42ea
Reno Muijsenberg <490412@student.fontys.nl>
2022-11-14 12:26:54 +0100

    Show multiple dates

--- src/app/festival-detail/festival-detail.component.ts
+++ src/app/festival-detail/festival-detail.component.ts
@@ -19,14 +19,50 @@ export class FestivalDetailComponent implements OnInit {
   
   splitDates() {
     if (this.festival.festivalDate.includes("-")) {
-      this.dates.push(this.festival.festivalDate. split(" - ")[0]);
-      this.dates.push(this.festival.festivalDate. split(" - ")[1]);
+      const startDate = this.festival.festivalDate. split(" - ")[0];
+      const endDate = this.festival.festivalDate. split(" - ")[1];
+      this.getDaysArray(startDate, endDate)
     }
     else {
       this.dates.push(this.festival.festivalDate);
     }
   }
 
+  getDaysArray(start: string, end: string) {
+    const dates: string[] = [];
+    let newStart = new Date(start);
+    newStart.setFullYear(this.getYear(start));
+    
+    let newEnd = new Date(end);
+    newEnd.setFullYear(this.getYear(end));
+    
+    const diff = Math.ceil((newEnd.getTime() - newStart.getTime()) / (1000 * 3600 * 24));
+    
+    for (let i = 0; i <= diff; i++) {
+      dates.push(newStart.toLocaleString('default', { day: '2-digit', month: 'short' }) + " " + newStart.getFullYear())
+      newStart.setDate(newStart.getDate() + 1)
+    }
+    
+    console.log(dates)
+    
+    this.dates = dates;
+  };
+  
+  getYear(date: string): number {
+    const today = new Date();
+    const thisYear = today.getFullYear();
+    const currentDate = new Date(date);
+    currentDate.setFullYear(thisYear)
+    
+    today.setHours(0, 0, 0, 0);
+    
+    if (currentDate < today) {
+      return new Date(new Date().setFullYear(new Date().getFullYear() + 1)).getFullYear();
+    }
+
+    return thisYear;
+  }
+  
   getFestival() {
     this.festival = this.festivalService.getSpecificFestival(this.festivalName);
     this.festival.subscribe((x: Festival) => {
